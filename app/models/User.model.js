390cb12647d4317febad688e0f4d5df25d43471e
Sebastian Petak <sebastianpetak@gmail.com>
2017-05-27 13:35:48 -0500

    Create User.model Password is not yet encrypted.

---
+++ app/models/User.model.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+	email: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	birthdayMonth: String,
+	birthdayDay: String,
+	gender: String,
+	radio: String,
+	check: Boolean
+});
+
+module.exports = mongoose.model('User', UserSchema);
