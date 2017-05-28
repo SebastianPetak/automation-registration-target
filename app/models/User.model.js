5bea2fbbfc038500a9c83582cc5fe79508938dd3
SebastianPetak <sebastian.petak@gmail.com>
2017-05-27 22:36:27 -0500

    Encrypt the users password Users password is going into the database unencrypted. Encrypt the users password.

--- app/models/User.model.js
+++ app/models/User.model.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
 
 var UserSchema = new Schema({
 	email: {
@@ -14,6 +15,16 @@ var UserSchema = new Schema({
 	gender: String,
 	radio: String,
 	check: Boolean
+});
+
+UserSchema.pre('save', function(next) {
+	var user = this;
+	bcrypt.hash(user.password, 10).then(function(hash) {
+		user.password = hash;
+		next();
+	}).catch(function(err) {
+		return next(err);
+	});
 });
 
 module.exports = mongoose.model('User', UserSchema);
